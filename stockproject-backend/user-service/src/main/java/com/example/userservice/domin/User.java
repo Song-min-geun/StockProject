46452005bc6365f3a8c2780a1263b27fb69cadb6
geun <minkeon44@naver.com>
2025-06-16 15:28:38 +0900

    feature/add-user-entity

---
+++ stockproject-backend/user-service/src/main/java/com/example/userservice/domin/User.java
@@ -0,0 +1,21 @@
+package com.example.userservice.domin;
+
+import jakarta.persistence.*;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor( access = AccessLevel.PROTECTED)
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+}
