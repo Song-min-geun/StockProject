a25a565cd17426e45660ab7e5cd32217148688ee
geun <minkeon44@naver.com>
2025-06-16 15:28:51 +0900

    feature/add-user-service

---
+++ stockproject-backend/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -0,0 +1,14 @@
+package com.example.userservice.service;
+
+import com.example.userservice.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class UserServiceImpl implements UserService{
+
+
+    private final UserRepository userRepository;
+    private final
+}
