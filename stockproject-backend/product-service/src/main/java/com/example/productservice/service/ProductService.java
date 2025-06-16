88d694da1c8bc654d3bb87748e5c6e71982119b9
geun <minkeon44@naver.com>
2025-06-16 15:35:45 +0900

    fix/add-product-service

--- stockproject-backend/product-service/src/main/java/com/example/productservice/service/ProductService.java
+++ stockproject-backend/product-service/src/main/java/com/example/productservice/service/ProductService.java
@@ -3,9 +3,6 @@ package com.example.productservice.service;
 import com.example.productservice.domain.Product;
 import com.example.productservice.dto.request.ProductRegistrationRequest;
 import com.example.productservice.dto.response.ProductResponseDto;
-import com.example.productservice.repository.ProductRepository;
-import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
