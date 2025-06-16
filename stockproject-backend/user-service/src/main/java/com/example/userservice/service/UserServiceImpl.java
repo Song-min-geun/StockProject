a1cb211d4ddc0ff773b6aee9dd9756929877a107
geun <minkeon44@naver.com>
2025-06-16 15:35:37 +0900

    fix/add-user-service

--- stockproject-backend/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ stockproject-backend/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -7,8 +7,5 @@ import org.springframework.stereotype.Service;
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
-
-
     private final UserRepository userRepository;
-    private final
 }
