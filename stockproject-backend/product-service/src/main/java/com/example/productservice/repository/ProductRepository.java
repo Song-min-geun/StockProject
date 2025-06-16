93685497aea587323113ec45ac2012f5705488b7
geun <minkeon44@naver.com>
2025-06-16 15:16:05 +0900

    feature/add-product-repository

---
+++ stockproject-backend/product-service/src/main/java/com/example/productservice/repository/ProductRepository.java
@@ -0,0 +1,13 @@
+package com.example.productservice.repository;
+
+import com.example.productservice.domain.Product;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product,Long> {
+    List<Product> findAllByIdIn (List<Long> Ids);
+}
