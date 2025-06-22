d9d18da9f5f197fd42de91db9054bbbc62b4ad23
geun <minkeon44@naver.com>
2025-06-22 20:07:20 +0900

    feature/add-userservice-service

--- stockproject-backend/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
+++ stockproject-backend/user-service/src/main/java/com/example/userservice/service/UserServiceImpl.java
@@ -1,11 +1,50 @@
 package com.example.userservice.service;
 
+import com.example.userservice.jwt.JwtTokenProvider;
+import com.example.userservice.domin.User;
+import com.example.userservice.dto.LoginRequestDto;
+import com.example.userservice.dto.SignUpRequestDto;
+import com.example.userservice.dto.TokenResponseDto;
 import com.example.userservice.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
 @RequiredArgsConstructor
 public class UserServiceImpl implements UserService{
     private final UserRepository userRepository;
+    private final PasswordEncoder passwordEncoder;
+    private final JwtTokenProvider jwtTokenProvider;
+
+    @Override
+    @Transactional
+    public void signUp(SignUpRequestDto requestDto){
+        if(userRepository.findByEmail(requestDto.email()).isPresent()){
+            throw new IllegalArgumentException("이미 사용중인 이메일입니다.");
+        }
+        String encodedPassword = passwordEncoder.encode(requestDto.password());
+
+        User user = new User(requestDto.email(),encodedPassword);
+        userRepository.save(user);
+    }
+
+    @Override
+    @Transactional(readOnly = true)
+    public TokenResponseDto login(LoginRequestDto requestDto) {
+        // 1. 이메일로 사용자 조회
+        User user = userRepository.findByEmail(requestDto.email())
+                .orElseThrow(() -> new IllegalArgumentException("사용자를 찾을 수 없습니다."));
+
+        // 2. 비밀번호 일치 여부 확인
+        if (!passwordEncoder.matches(requestDto.password(), user.getPassword())) {
+            throw new IllegalArgumentException("비밀번호가 일치하지 않습니다.");
+        }
+
+        // 3. JWT 생성
+        String accessToken = jwtTokenProvider.generateToken(user.getEmail());
+
+        return new TokenResponseDto(accessToken);
+    }
 }
