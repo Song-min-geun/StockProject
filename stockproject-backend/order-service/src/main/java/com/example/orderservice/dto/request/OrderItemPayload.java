544132c35b92c0881957cb2005d7431976fbddf3
geun <minkeon44@naver.com>
2025-07-01 12:54:16 +0900

    fix/local-dto-to-common-dto

--- stockproject-backend/order-service/src/main/java/com/example/orderservice/dto/request/OrderItemPayload.java
+++
@@ -1,8 +0,0 @@
-package com.example.orderservice.dto.request;
-
-// OrderCreatedEvent에 포함될 상품 정보
-public record OrderItemPayload(
-        Long productId,
-        Integer quantity,
-        Long price // 주문 당시의 개별 단가
-) {}
--- stockproject-backend/stock-service/src/main/java/com/example/stockservice/dto/OrderItemPayload.java
+++
@@ -1,7 +0,0 @@
-package com.example.stockservice.dto;
-
-public record OrderItemPayload(
-        Long productId,
-        Integer quantity,
-        Long price
-) {}
