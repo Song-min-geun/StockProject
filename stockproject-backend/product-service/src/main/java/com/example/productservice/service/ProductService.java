91ada226b17774eaa6c16558e9f6844fd9a0ae42
geun <minkeon44@naver.com>
2025-07-28 21:29:30 +0900

    refactor/renaming-productRegistrationRequestDto

--- stockproject-backend/product-service/src/main/java/com/example/productservice/service/ProductService.java
+++ stockproject-backend/product-service/src/main/java/com/example/productservice/service/ProductService.java
@@ -1,17 +1,19 @@
 package com.example.productservice.service;
 
 import com.example.productservice.domain.Product;
-import com.example.productservice.dto.request.ProductRegistrationRequest;
+import com.example.productservice.dto.request.ProductRegistrationRequestDto;
 import com.example.productservice.dto.response.ProductResponseDto;
 
 import java.util.List;
 
 public interface ProductService {
-    Product registerProduct(ProductRegistrationRequest request);
+    Product registerProduct(ProductRegistrationRequestDto request);
 
     // 단건 조회를 위한 메서드 추가
     ProductResponseDto findProductById(String productId);
 
     // ID 타입을 Long에서 String으로 변경
     List<ProductResponseDto> findProductsByIds(List<String> ids);
+
+    void deleteProduct(String productId);
 }
--- stockproject-backend/product-service/src/main/java/com/example/productservice/service/ProductServiceImpl.java
+++ stockproject-backend/product-service/src/main/java/com/example/productservice/service/ProductServiceImpl.java
@@ -1,12 +1,13 @@
 package com.example.productservice.service;
 
+import com.example.dto.ProductCreatedEvent;
 import com.example.productservice.domain.Product;
-import com.example.productservice.dto.request.ProductRegistrationRequest;
+import com.example.productservice.dto.request.ProductRegistrationRequestDto;
 import com.example.productservice.dto.response.ProductResponseDto;
 import com.example.productservice.repository.ProductRepository;
 import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.core.KafkaTemplate;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -16,19 +17,31 @@ public class ProductServiceImpl implements ProductService {
 
     private final ProductRepository productRepository;
 
+    private final KafkaTemplate<String, Object> kafkaTemplate;
+
     @Override
-    @Transactional
-    public Product registerProduct(ProductRegistrationRequest request) {
+
+    public Product registerProduct(ProductRegistrationRequestDto request) {
         Product product = Product.builder()
                 .name(request.name())
                 .price(request.price())
                 .stock(request.initialStock())
                 .build();
-        return productRepository.save(product);
+        Product savedProduct =  productRepository.save(product);
+
+        ProductCreatedEvent event = new ProductCreatedEvent(savedProduct.getId(),savedProduct.getStock());
+        kafkaTemplate.send("product-created", event);
+
+        return savedProduct;
     }
 
     @Override
-    @Transactional(readOnly = true)
+    public void deleteProduct(String productId){
+        productRepository.deleteById(productId);
+        kafkaTemplate.send("product-deleted",productId);
+    }
+
+    @Override
     public ProductResponseDto findProductById(String productId) {
         // Repository의 내장 메서드인 findById 사용
         Product product = productRepository.findById(productId)
@@ -37,7 +50,7 @@ public class ProductServiceImpl implements ProductService {
     }
 
     @Override
-    @Transactional(readOnly = true)
+
     public List<ProductResponseDto> findProductsByIds(List<String> ids) { // ID 타입을 String으로 수정
         // Repository의 내장 메서드인 findAllById 사용
         List<Product> products = productRepository.findAllById(ids);
