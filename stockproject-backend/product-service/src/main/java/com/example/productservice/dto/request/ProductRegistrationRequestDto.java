b29d0549eefba23c6fb60dcef76dcd4f90792ee9
geun <minkeon44@naver.com>
2025-07-28 21:28:05 +0900

    refactor/renaming-dto

--- stockproject-backend/product-service/src/main/java/com/example/productservice/dto/request/ProductRegistrationRequestDto.java
+++ stockproject-backend/product-service/src/main/java/com/example/productservice/dto/request/ProductRegistrationRequestDto.java
@@ -1,7 +1,7 @@
 package com.example.productservice.dto.request;
 
 //제품 처음 등록할시 필요한 dto
-public record ProductRegistrationRequest(
+public record ProductRegistrationRequestDto(
         String name,
         Long price,
         int initialStock
